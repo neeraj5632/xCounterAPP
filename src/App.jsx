4d7b14aac84ecdcfb1bcfe350e23e0028a96abc9
Neeraj <neerajpande08@gmail.com>
2025-10-30 15:34:03 +0530

    inital commit of couter app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => setCount(count + 1);
+  const handleDecrement = () => setCount(count - 1);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Count: {count}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
